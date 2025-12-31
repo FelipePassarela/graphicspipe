a7a3625306201acfd6c0f00a9379a690da2a5893
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2025-12-31 07:57:50 -0300

    feat: implement input state handling
    Add InputState class to manage keyboard input and update camera movement in main loop.

--- src/graphicspipe/main.py
+++ src/graphicspipe/main.py
@@ -3,13 +3,18 @@ import sys
 import time
 
 import numpy as np
+from pynput import keyboard
 
 from graphicspipe import math
+from graphicspipe.input_state import InputState
 from graphicspipe.torus_controller import TorusController
 
 SCREEN_W = 120
 SCREEN_H = 40
 FRAME_DELAY = 1 / 60.0
+
+MOVE_SPEED = 2.0
+CAMERA_ROTATION_SPEED = 45.0
 
 
 def main() -> None:
@@ -31,9 +36,43 @@ def main() -> None:
         "far": -100.0,
     }
 
+    input_state = InputState()
+    key_listener = keyboard.Listener(
+        on_press=input_state.on_press, on_release=input_state.on_release
+    )
+    key_listener.start()
+
     os.system("cls" if os.name == "nt" else "clear")
+
     while True:
         model["rotation"][1] += np.radians(180) * FRAME_DELAY
+
+        camera["pitch"] = np.clip(camera["pitch"], -89, 89)
+        forward = math.forward(np.radians(camera["yaw"]), np.radians(camera["pitch"]))
+        left = np.cross(camera["up"], forward)
+        left /= np.linalg.norm(left)
+
+        if input_state.is_pressed("w"):
+            camera["eye"] += forward * MOVE_SPEED * FRAME_DELAY
+        if input_state.is_pressed("s"):
+            camera["eye"] -= forward * MOVE_SPEED * FRAME_DELAY
+        if input_state.is_pressed("a"):
+            camera["eye"] += left * MOVE_SPEED * FRAME_DELAY
+        if input_state.is_pressed("d"):
+            camera["eye"] -= left * MOVE_SPEED * FRAME_DELAY
+
+        if input_state.is_pressed(keyboard.Key.left):
+            camera["yaw"] += CAMERA_ROTATION_SPEED * FRAME_DELAY
+        if input_state.is_pressed(keyboard.Key.right):
+            camera["yaw"] -= CAMERA_ROTATION_SPEED * FRAME_DELAY
+        if input_state.is_pressed(keyboard.Key.up):
+            camera["pitch"] += CAMERA_ROTATION_SPEED * FRAME_DELAY
+        if input_state.is_pressed(keyboard.Key.down):
+            camera["pitch"] -= CAMERA_ROTATION_SPEED * FRAME_DELAY
+
+        if input_state.is_pressed(keyboard.Key.esc):
+            key_listener.stop()
+            exit()
 
         model["mesh"] = torus_controller.update(model["mesh"])
 
@@ -45,8 +84,6 @@ def main() -> None:
         world_coords = model["mesh"] @ world_matrix  # objects in row vector format
 
         # view transformation
-        camera["pitch"] = np.clip(camera["pitch"], -89, 89)
-        forward = math.forward(np.radians(camera["yaw"]), np.radians(camera["pitch"]))
         target = camera["eye"] + forward
         view_matrix = math.look_at(camera["eye"], target, camera["up"])
         view_coords = world_coords @ view_matrix
