814fa7bb76581e67c51c78b3eacfb75d99e55878
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2026-01-04 04:09:51 -0300

    feat: implement rasterization and lighting effects
    - Added rasterization functionality for face rendering
    - Added light source calculations for shading based on normals.
    - Render a plane for better visualization of the new effects
    - Updated camera controls to use delta time for smoother movement.
    - Refactored display function to include FPS information.

--- src/graphicspipe/main.py
+++ src/graphicspipe/main.py
@@ -7,11 +7,10 @@ from pynput import keyboard
 
 from graphicspipe import math, mesh
 from graphicspipe.input_state import InputState
-from graphicspipe.torus_controller import TorusController
 
 SCREEN_W = 120
 SCREEN_H = 40
-FRAME_DELAY = 1 / 60.0
+TARGET_FRAME_TIME = 1.0 / 60.0
 
 MOVE_SPEED = 2.0
 CAMERA_ROTATION_SPEED = 45.0
@@ -27,19 +26,26 @@ def main() -> None:
         "faces": faces,
         "translation": np.array([0.0, 0.0, 0.0]),
         "scale": np.array([1.0, 1.0, 1.0]),
-        "rotation": np.array([0.0, 0.0, 0.0]),
+        "rotation": np.array([90.0, 0.0, 0.0]),
     }
-    # model["scale"] /= np.max(np.abs(model["mesh"][:, :3]))  # normalize size
+    model["scale"] /= np.max(np.abs(model["mesh"][:, :3]))  # normalize size
 
     camera = {
-        "yaw": 0.0,
-        "pitch": 0.0,
-        "eye": np.array([0.0, 0.0, -3.0]),  # move back to see the object
+        "yaw": -75.0,
+        "pitch": 10.0,
+        "eye": np.array([-1.2, 0.0, -0.3]),  # move back to see the object
         "up": np.array([0.0, 1.0, 0.0]),
         "near": 0.1,
         "far": 100.0,
         "fov": 60.0,
     }
+
+    light_source = {
+        "position": np.array([-5.0, 5.0, -5.0]),
+        "direction": np.array([0.0, 0.0, 1.0]),
+    }
+    light_source["direction"] /= np.array([0.0, 0.0, 0.0]) - light_source["position"]
+    light_source["direction"] /= np.linalg.norm(light_source["direction"])
 
     input_state = InputState()
     key_listener = keyboard.Listener(
@@ -48,35 +54,39 @@ def main() -> None:
     key_listener.start()
 
     os.system("cls" if os.name == "nt" else "clear")
+    last_time = time.time()
 
     while True:
-        model["rotation"][1] += np.radians(180) * FRAME_DELAY
+        now = time.time()
+        dt = now - last_time
+        last_time = now
+        dt = min(dt, 0.1)  # avoid large jumps
 
         forward = math.forward(np.radians(camera["yaw"]), np.radians(camera["pitch"]))
         right = np.cross(camera["up"], forward)
         right /= np.linalg.norm(right)
 
         if input_state.is_pressed("w"):
-            camera["eye"] += forward * MOVE_SPEED * FRAME_DELAY
+            camera["eye"] += forward * MOVE_SPEED * dt
         if input_state.is_pressed("s"):
-            camera["eye"] -= forward * MOVE_SPEED * FRAME_DELAY
+            camera["eye"] -= forward * MOVE_SPEED * dt
         if input_state.is_pressed("a"):
-            camera["eye"] -= right * MOVE_SPEED * FRAME_DELAY
+            camera["eye"] -= right * MOVE_SPEED * dt
         if input_state.is_pressed("d"):
-            camera["eye"] += right * MOVE_SPEED * FRAME_DELAY
+            camera["eye"] += right * MOVE_SPEED * dt
 
         if input_state.is_pressed(keyboard.Key.left):
-            camera["yaw"] += CAMERA_ROTATION_SPEED * FRAME_DELAY
+            camera["yaw"] += CAMERA_ROTATION_SPEED * dt
         if input_state.is_pressed(keyboard.Key.right):
-            camera["yaw"] -= CAMERA_ROTATION_SPEED * FRAME_DELAY
+            camera["yaw"] -= CAMERA_ROTATION_SPEED * dt
         if input_state.is_pressed(keyboard.Key.up):
-            camera["pitch"] += CAMERA_ROTATION_SPEED * FRAME_DELAY
+            camera["pitch"] += CAMERA_ROTATION_SPEED * dt
         if input_state.is_pressed(keyboard.Key.down):
-            camera["pitch"] -= CAMERA_ROTATION_SPEED * FRAME_DELAY
+            camera["pitch"] -= CAMERA_ROTATION_SPEED * dt
         if input_state.is_pressed("+"):
-            camera["fov"] = min(150.0, camera["fov"] + FOV_SPEED * FRAME_DELAY)
+            camera["fov"] = min(150.0, camera["fov"] + FOV_SPEED * dt)
         if input_state.is_pressed("-"):
-            camera["fov"] = max(60.0, camera["fov"] - FOV_SPEED * FRAME_DELAY)
+            camera["fov"] = max(60.0, camera["fov"] - FOV_SPEED * dt)
 
         if input_state.is_pressed(keyboard.Key.esc):
             key_listener.stop()
@@ -116,34 +126,63 @@ def main() -> None:
         clip_coords = clip_coords[:, :3] / clip_coords[:, 3:4]
 
         # viewport transformation
-        sx = (clip_coords[:, 0] + 1) * SCREEN_W / 2.0
-        sy = (1 - clip_coords[:, 1]) * SCREEN_H / 2.0
-        screen_coords = np.stack([sx, sy], axis=1)
-        screen_coords = np.round(screen_coords).astype(np.int32)
+        viewport = np.full((SCREEN_H, SCREEN_W), " ")
 
-        viewport = np.full((SCREEN_H, SCREEN_W), ord(" "), dtype=np.uint8)
-        xs = screen_coords[:, 0]
-        ys = screen_coords[:, 1]
-        mask = (xs >= 0) & (xs < SCREEN_W) & (ys >= 0) & (ys < SCREEN_H)
-        viewport[ys[mask], xs[mask]] = ord("*")
+        for face in model["faces"]:
+            try:
+                v1_idx, v2_idx, v3_idx = face[:, 0]
+                v1 = clip_coords[v1_idx]
+                v2 = clip_coords[v2_idx]
+                v3 = clip_coords[v3_idx]
+            except IndexError:
+                continue
+
+            center = (v1 + v2 + v3) / 3.0
+            sx = int((center[0] + 1) * SCREEN_W / 2.0)
+            sy = int((1 - center[1]) * SCREEN_H / 2.0)
+            if not (0 <= sx < SCREEN_W and 0 <= sy < SCREEN_H):
+                continue
+
+            n1_idx, n2_idx, n3_idx = face[:, 1]
+            n1_intesity = np.dot(model["normals"][n1_idx], light_source["direction"])
+            n2_intesity = np.dot(model["normals"][n2_idx], light_source["direction"])
+            n3_intesity = np.dot(model["normals"][n3_idx], light_source["direction"])
+            intensity = (n1_intesity + n2_intesity + n3_intesity) / 3.0
+            if intensity < 0:  # backface culling
+                continue
+
+            shade_chars = " ░▒▓█"
+            shade_index = min(int(len(shade_chars) * intensity), len(shade_chars) - 1)
+            viewport[sy, sx] = shade_chars[shade_index]
 
         display(viewport, camera=camera)
 
-        time.sleep(FRAME_DELAY)
+        elapsed = time.time() - now
+        sleep_time = TARGET_FRAME_TIME - elapsed
+        if sleep_time > 0:
+            time.sleep(sleep_time)
 
 
-def display(viewport: np.ndarray, camera: dict = None):
-    lines = [bytes(row).decode("ascii") for row in viewport]
+def display(viewport: np.ndarray, camera: dict = None, dt: float = None) -> None:
+    lines = ["".join(c for c in row) for row in viewport]
+    message = "Graphicspipe - Press ESC to quit"
+    lines[SCREEN_H - 1] = message.ljust(SCREEN_W)
+
+    debug_info = ""
     if camera is not None:
         camera_x = camera["eye"][0]
         camera_y = camera["eye"][1]
         camera_z = camera["eye"][2]
-        lines.insert(
-            0,
+        debug_info += (
             f"Camera Position: ({camera_x:.2f}, {camera_y:.2f}, {camera_z:.2f}) "
             f"Yaw: {camera['yaw']:.2f} Pitch: {camera['pitch']:.2f} "
-            f"FOV: {camera['fov']:.2f}",
+            f"FOV: {camera['fov']:.2f} "
         )
+    if dt is not None:
+        fps = 1.0 / dt if dt > 0 else 0.0
+        debug_info += f"FPS: {fps:.2f}"
+    lines[0] = debug_info.ljust(SCREEN_W)
+
     frame = "\n".join(lines)
     sys.stdout.write("\x1b[H" + frame)
     sys.stdout.flush()
